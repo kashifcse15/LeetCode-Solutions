a3b9ff6ed4c94f9d3be32c22756e840a3055bf43
kashifcse15 <kashifshaikh.cse@gmail.com>
2026-07-05 17:03:10 +0530

    Add solution for finding eventual safe states in graph
    Implement a solution to find eventual safe states in a directed graph using reverse graph and out-degree tracking.

--- Graph/802. Find Eventual Safe States.cpp
+++ Graph/802. Find Eventual Safe States.cpp
@@ -1,1 +1,40 @@
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<vector<int>>revGraph(n);
+        vector<int>outDegree(n);
+        vector<int>ans;
 
+        for(int i=0;i<n;i++){
+            outDegree[i]=graph[i].size();
+            for(int neigh:graph[i]){
+                revGraph[neigh].push_back(i);
+            }
+        }
+
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(outDegree[i]==0){
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+
+            for(int parent:revGraph[node]){
+                outDegree[parent]--;
+
+                if(outDegree[parent]==0){
+                    q.push(parent);
+                }
+            }
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+
+    }
+};

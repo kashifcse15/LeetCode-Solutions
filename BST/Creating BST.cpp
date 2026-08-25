0c448d554e35a5a6e1aae41519652ef82cba1819
kashifcse15 <kashifshaikh.cse@gmail.com>
2026-08-25 20:47:06 +0530

    Implement binary search tree with insertion and traversal

--- BST/Creating BST.cpp
+++ BST/Creating BST.cpp
@@ -1,1 +1,64 @@
+#include <iostream>
+using namespace std;
 
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value) {
+        data = value;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+Node* insert(Node* root, int value) {
+
+    // If tree is empty
+    if (root == NULL) {
+        return new Node(value);
+    }
+
+    // Smaller values go to left
+    if (value < root->data) {
+        root->left = insert(root->left, value);
+    }
+
+    // Greater values go to right
+    else if (value > root->data) {
+        root->right = insert(root->right, value);
+    }
+
+    return root;
+}
+
+// Inorder traversal
+void inorder(Node* root) {
+
+    if (root == NULL)
+        return;
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int main() {
+
+    Node* root = NULL;
+
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 70);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 60);
+    insert(root, 80);
+
+    cout << "Inorder Traversal: ";
+    inorder(root);
+
+    return 0;
+}

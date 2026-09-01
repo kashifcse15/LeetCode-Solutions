c2a57cfd8820bcfbc8f22c8f6295c56ba81cb77f
kashifcse15 <kashifshaikh.cse@gmail.com>
2026-09-01 22:09:57 +0530

    Implement flattening of a doubly linked list

--- Tree/Flatten a Doubly Linked List.cpp
+++ Tree/Flatten a Doubly Linked List.cpp
@@ -1,1 +1,43 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
 
+class Solution {
+public:
+    Node* flatten(Node* head) {
+        if (!head) return head;
+
+        Node* curr = head;
+
+        while (curr) {
+            if (curr->child) {
+                Node* next = curr->next;  // store next
+                Node* child = flatten(curr->child);  // flatten child list
+
+                
+
+                // Go to the tail of the child list
+                Node* temp = child;
+                while (temp->next) {
+                    temp = temp->next;
+                }
+
+                // Connect tail of child to next
+                temp->next = next;
+                if (next) next->prev = temp;
+            }
+
+            curr = curr->next;  // move forward
+        }
+
+        return head;
+    }
+};
+;

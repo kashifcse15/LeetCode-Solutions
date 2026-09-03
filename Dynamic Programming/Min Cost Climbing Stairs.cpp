c342666b85a3bf605adfbbb1133b04da58b236d2
kashifcse15 <kashifshaikh.cse@gmail.com>
2026-09-03 20:19:14 +0530

    Add optimized minCostClimbingStairs function
    Implemented an optimized solution for calculating the minimum cost to climb stairs using dynamic programming.

--- Dynamic Programming/Min Cost Climbing Stairs.cpp
+++ Dynamic Programming/Min Cost Climbing Stairs.cpp
@@ -1,1 +1,14 @@
+class Solution { //This one is the most Optimized One , T(n), S(1)
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int prev2=0;
+        int prev1=0;
 
+        for(int i=2;i<=cost.size();i++){
+            int curr=min(cost[i-1]+prev1, cost[i-2]+prev2);
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
+    }
+};

c1e60ad024974f056b94bbc86f4fd82660591146
kashifcse15 <kashifshaikh.cse@gmail.com>
2026-09-05 19:44:27 +0530

    Add memoization to frogJump function
    Implemented memoization for frog jump problem.

--- Dynamic Programming/Frog Jump.cpp
+++ Dynamic Programming/Frog Jump.cpp
@@ -17,3 +17,29 @@ int frogJump(int i, int height[]) {
 
     return min(oneStep, twoStep);
 }
+
+
+int frogJump(int i, int height[], int dp[]) {
+//MEMOISATION / TOP DOWN
+    if (i == 0)
+        return 0;
+
+    if (dp[i] != -1)
+        return dp[i];
+
+    int oneStep =
+        frogJump(i - 1, height, dp)
+        + abs(height[i] - height[i - 1]);
+
+    int twoStep = INT_MAX;
+
+    if (i > 1) {
+        twoStep =
+            frogJump(i - 2, height, dp)
+            + abs(height[i] - height[i - 2]);
+    }
+
+    dp[i] = min(oneStep, twoStep);
+
+    return dp[i];
+}

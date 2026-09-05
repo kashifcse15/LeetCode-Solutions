2c60b7f7df5b99c95dfc5353e94688fe50a01744
kashifcse15 <kashifshaikh.cse@gmail.com>
2026-09-05 19:43:32 +0530

    Add recursive frog jump function
    Implement frog jump function using recursion to calculate minimum cost.

--- Dynamic Programming/Frog Jump.cpp
+++ Dynamic Programming/Frog Jump.cpp
@@ -1,1 +1,19 @@
+int frogJump(int i, int height[]) {
+  //BY RECURSION
+    if (i == 0)
+        return 0;
 
+    int oneStep =
+        frogJump(i - 1, height)
+        + abs(height[i] - height[i - 1]);
+
+    int twoStep = INT_MAX;
+
+    if (i > 1) {
+        twoStep =
+            frogJump(i - 2, height)
+            + abs(height[i] - height[i - 2]);
+    }
+
+    return min(oneStep, twoStep);
+}

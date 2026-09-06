ecb5a3f3e81e1163ca75a457d1428d10537964ee
kashifcse15 <kashifshaikh.cse@gmail.com>
2026-09-06 22:38:34 +0530

    Add serialize method to Codec class
    Implement serialization method for binary tree.

--- Tree/Serialize and Deserialize Binary Tree.cpp
+++ Tree/Serialize and Deserialize Binary Tree.cpp
@@ -1,1 +1,35 @@
+/*
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+// Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+    if(root == NULL) return "";
+    string ans = "";
+    queue<TreeNode*> q;
+    q.push(root);
 
+    while(!q.empty()) {
+        TreeNode* node = q.front();
+        
+        if(node == NULL) {
+            ans += "# ";
+        }
+        else {
+            ans += to_string(node->val) + " ";
+            q.push(node->left);
+            q.push(node->right);
+        }
+    }
+
+    return ans;  //1 2 3 # # 4 5 # # # #
+}
+
+    

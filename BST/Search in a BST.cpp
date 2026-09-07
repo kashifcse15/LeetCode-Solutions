9dd38c3cc34e973cf2cc88563021d6c11d631202
kashifcse15 <kashifshaikh.cse@gmail.com>
2026-09-07 18:44:01 +0530

    Implement null check in searchBST method
    Add null check for root in searchBST function

--- BST/Search in a BST.cpp
+++ BST/Search in a BST.cpp
@@ -13,6 +13,9 @@ class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
 
+          if (root == NULL)
+            return NULL;
+
         if (root->val == val)
             return root;
 

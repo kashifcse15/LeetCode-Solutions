adc87ed35a0c8007cf5ca21e726caa33d8ab9bcb
kashifcse15 <kashifshaikh.cse@gmail.com>
2026-09-10 21:24:47 +0530

    Optimize House Robber solution to O(1) space
    Optimized space complexity of the House Robber solution to O(1) by using two variables to track previous values instead of a full array.

--- Dynamic Programming/House Robber.cpp
+++ Dynamic Programming/House Robber.cpp
@@ -44,3 +44,28 @@ public:
 };
 
 
+//SPACE OPTIMIZE TO O(1)
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+
+        int prev2 = 0;
+        int prev1 = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            int rob = nums[i] + prev2;
+            int skip = prev1;
+
+            int curr = max(rob, skip);
+
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+};
+
+
+

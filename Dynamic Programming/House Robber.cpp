654585701ff9bbb388f8f111b9ed9bf80438b743
kashifcse15 <kashifshaikh.cse@gmail.com>
2026-09-10 21:23:01 +0530

    Update House Robber.cpp

--- Dynamic Programming/House Robber.cpp
+++ Dynamic Programming/House Robber.cpp
@@ -1,1 +1,19 @@
+class Solution { //PURE RECURSION
+public:
+    int solve(int i, vector<int>& nums) {
+        
+        if (i < 0)
+            return 0;
 
+        int rob = nums[i] + solve(i - 2, nums);
+        int skip = solve(i - 1, nums);
+
+        return max(rob, skip);
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        return solve(n - 1, nums);
+    }
+};

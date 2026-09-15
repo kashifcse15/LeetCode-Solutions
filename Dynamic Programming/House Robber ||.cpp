b4e43d7da21533102cb36537190b2a2c754a24e1
kashifcse15 <kashifshaikh.cse@gmail.com>
2026-09-15 23:33:40 +0530

    Implement memoization in House Robber solution
    Added memoization approach for the House Robber problem to optimize the solution.

--- Dynamic Programming/House Robber ||.cpp
+++ Dynamic Programming/House Robber ||.cpp
@@ -20,3 +20,36 @@ public:
         return max(case1, case2);
     }
 };
+
+//MEMORIZATION 
+class Solution {
+public:
+    int solve(vector<int>& nums, int i, int end,
+              vector<int>& dp) {
+        
+        if (i > end) return 0;
+
+        if (dp[i] != -1) return dp[i];
+
+        int rob = nums[i] + solve(nums, i + 2, end, dp);
+        int skip = solve(nums, i + 1, end, dp);
+
+        return dp[i] = max(rob, skip);
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        if (n == 1) return nums[0];
+
+        vector<int> dp(n, -1);
+
+        int case1 = solve(nums, 0, n - 2);
+
+        fill(dp.begin(), dp.end(), -1);
+
+        int case2 = solve(nums, 1, n - 1);
+
+        return max(case1, case2);
+    }
+};

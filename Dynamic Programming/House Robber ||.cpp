63279c4ba34afce1b5adb34d697982fd9ee46afa
kashifcse15 <kashifshaikh.cse@gmail.com>
2026-09-15 23:34:40 +0530

    Add space-optimized solution for House Robber
    Implemented space optimization for the House Robber problem using a linear approach.

--- Dynamic Programming/House Robber ||.cpp
+++ Dynamic Programming/House Robber ||.cpp
@@ -89,3 +89,35 @@ public:
         return max(case1, case2);
     }
 };
+
+//SPACE OPTIMIZATION 
+class Solution {
+public:
+    int robLinear(vector<int>& nums, int start, int end) {
+        int prev2 = 0;
+        int prev1 = 0;
+
+        for (int i = start; i <= end; i++) {
+            int rob = nums[i] + prev2;
+            int skip = prev1;
+
+            int curr = max(rob, skip);
+
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        if (n == 1) return nums[0];
+
+        int case1 = robLinear(nums, 0, n - 2);
+        int case2 = robLinear(nums, 1, n - 1);
+
+        return max(case1, case2);
+    }
+};

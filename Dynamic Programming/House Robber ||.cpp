2ec3e22a7e88b75c99f90613c608c63624af6cb2
kashifcse15 <kashifshaikh.cse@gmail.com>
2026-09-15 23:34:09 +0530

    Implement tabulation method for House Robber problem

--- Dynamic Programming/House Robber ||.cpp
+++ Dynamic Programming/House Robber ||.cpp
@@ -53,3 +53,39 @@ public:
         return max(case1, case2);
     }
 };
+
+//TABULATION 
+class Solution {
+public:
+    int robLinear(vector<int>& nums, int start, int end) {
+        int n = end - start + 1;
+
+        vector<int> dp(n, 0);
+
+        dp[0] = nums[start];
+
+        if (n == 1) return dp[0];
+
+        dp[1] = max(nums[start], nums[start + 1]);
+
+        for (int i = 2; i < n; i++) {
+            int rob = nums[start + i] + dp[i - 2];
+            int skip = dp[i - 1];
+
+            dp[i] = max(rob, skip);
+        }
+
+        return dp[n - 1];
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        if (n == 1) return nums[0];
+
+        int case1 = robLinear(nums, 0, n - 2);
+        int case2 = robLinear(nums, 1, n - 1);
+
+        return max(case1, case2);
+    }
+};

1b136b7cac91495581276fc36a6008a83ef29df2
kashifcse15 <kashifshaikh.cse@gmail.com>
2026-09-19 19:59:59 +0530

    Implemented Memoization Code

--- Dynamic Programming/Subset Sum Equal to K.cpp
+++ Dynamic Programming/Subset Sum Equal to K.cpp
@@ -1,3 +1,24 @@
+class Solution{ // RECURSION
+public:
+bool solve(int ind, int target, vector<int>& arr,  vector<vector<int>>&dp){
+   if(target==0) return true;
+   if(ind == 0) return arr[0]==target;
+   if(dp[ind][target]!=-1)  return dp[ind][target];
+   bool notPick=solve(ind-1, target, arr);
+   bool pick=false;
+   if(arr[ind] <= target){
+     pick=solve(ind-1, target-arr[ind],arr);
+   }
+
+  return dp[ind][target]=pick || notPick;
+}
+  bool subsetSum(vector<int>& arr, int k){
+    int n=arr.size();
+    return solve(n-1,k,arr);
+  }
+};
+
+// MEMOIZATION
 class Solution{
 public:
 bool solve(int ind, int target, vector<int>& arr){
@@ -14,6 +35,8 @@ bool solve(int ind, int target, vector<int>& arr){
 }
   bool subsetSum(vector<int>& arr, int k){
     int n=arr.size();
-    return solve(n-1,k,arr);
+     vector<vector<int>>dp(n,vector<int>(k+1,-1));
+    return solve(n-1,k,arr,dp);
   }
 };
+

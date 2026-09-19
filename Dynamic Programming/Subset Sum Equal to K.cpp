2fd4abd51e5bd610f93e9858acbfdf2218c5aa36
kashifcse15 <kashifshaikh.cse@gmail.com>
2026-09-19 19:54:32 +0530

    Implement subset sum solution using recursion

--- Dynamic Programming/Subset Sum Equal to K.cpp
+++ Dynamic Programming/Subset Sum Equal to K.cpp
@@ -1,1 +1,19 @@
+class Solution{
+public:
+bool solve(int ind, int target, vector<int>& arr){
+   if(target==0) return true;
+   if(ind == 0) return arr[0]==target;
 
+   bool notPick=solve(ind-1, target, arr);
+   bool pick=false;
+   if(arr[ind] <= target){
+     pick=solve(ind-1, target-arr[ind],arr);
+   }
+
+  return pick || notPick;
+}
+  bool subsetSum(vector<int>& arr, int k){
+    int n=arr.size();
+    return solve(n-1,k,arr);
+  }
+};

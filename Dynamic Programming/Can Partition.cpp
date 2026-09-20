eba9731ebfb1f6da99b3df310af874c5d99ba81a
kashifcse15 <kashifshaikh.cse@gmail.com>
2026-09-20 19:38:10 +0530

    Implement recursive solution for canPartition problem

--- Dynamic Programming/Can Partition.cpp
+++ Dynamic Programming/Can Partition.cpp
@@ -1,1 +1,41 @@
+class Solution {
+public:
 
+    bool solve(int i, int target, vector<int>& nums) {
+
+        // Target achieved
+        if(target == 0)
+            return true;
+
+        // No elements left
+        if(i == nums.size())
+            return false;
+
+        // Don't take
+        bool notTake = solve(i + 1, target, nums);
+
+        // Take
+        bool take = false;
+
+        if(nums[i] <= target)
+            take = solve(i + 1, target - nums[i], nums);
+
+        return take || notTake;
+    }
+
+    bool canPartition(vector<int>& nums) {
+
+        int sum = 0;
+
+        for(int x : nums)
+            sum += x;
+
+        // Equal partition impossible if total is odd
+        if(sum % 2 != 0)
+            return false;
+
+        int target = sum / 2;
+
+        return solve(0, target, nums);
+    }
+};
